9ba66c74186a03e5d0b3d1c45b4197c247d0702b
svetlana <fatinialux@yandex.ru>
2024-03-25 00:04:19 +0400

    refactor: delete unused code

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,15 @@ import MainPage from "./components/UI/pages/main-page";
 import AllProducts from "./components/UI/pages/all-products";
 import OneProduct from "./components/UI/pages/one-product";
 import NotFoundPage from "./components/UI/pages/not-found";
-import {Counter} from "./components/some";
-
 
 function App() {
   return (
     <Routes>
         <Route path="/" element={<MainPage />} />
-        <Route path="/some" element={<Counter />} />
         <Route path="/products">
             <Route index element={<AllProducts />} />
             <Route path=":id" element={<OneProduct />} />
         </Route>
-
         <Route path="*" element={<NotFoundPage />} />
     </Routes>
   )
